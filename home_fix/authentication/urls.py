ca8bbcd226670370bf5a55fe42e972e32e7f78f8
Jash Merchant <merchantjash@gmail.com>
2022-03-05 20:07:23 -0500

    Add Pricing Page and modify Homepage

--- home_fix/authentication/urls.py
+++ home_fix/authentication/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("homepage/", views.homepage_view, name="index"),
     path("set_location/<int:user_id>/", views.set_location, name="set_location"),
+    path("pricing/", views.pricing_view, name="pricing"),
     path("logout/", views.logout_view, name="logout"),
     path("", views.auth, name="auth"),
 ]
