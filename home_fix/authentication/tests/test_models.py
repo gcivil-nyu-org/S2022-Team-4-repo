3c0704126f34a3d9caa0c57961f180a165e4ab05
Yuhao <yz7726@nyu.edu>
2022-03-23 14:51:01 -0400

    test: superuser

--- home_fix/authentication/tests/test_models.py
+++ home_fix/authentication/tests/test_models.py
@@ -17,7 +17,7 @@ class TestModels(TestCase):
         self.lat = 1
 
     def test_user_create(self):
-        user = CustomUser.objects.create(
+        user = CustomUser.objects.create_user(
             email=self.email,
             first_name=self.first_name,
             last_name=self.last_name,
@@ -31,3 +31,20 @@ class TestModels(TestCase):
             lat=self.lat,
         )
         self.assertEquals(user.email, self.email)
+        user.delete()
+
+        superuser = CustomUser.objects.create_superuser(
+            email=self.email,
+            first_name=self.first_name,
+            last_name=self.last_name,
+            password=self.password,
+            gender=self.gender,
+            street=self.street,
+            state=self.state,
+            country=self.country,
+            zip=self.zip,
+            long=self.long,
+            lat=self.lat,
+        )
+        self.assertEquals(superuser.is_superuser, True)
+        superuser.delete()
