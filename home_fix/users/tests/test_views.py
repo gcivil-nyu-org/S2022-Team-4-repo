bfdc5c9c28fbeb1bf17c2ebef3e7920b5263d2f9
Yuhao <yz7726@nyu.edu>
2022-03-28 22:58:43 -0400

    test

--- home_fix/users/tests/test_views.py
+++ home_fix/users/tests/test_views.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from users.models import CustomUser
+
 
 class TestViews(TestCase):
     def setUp(self):
-        self.email = "user1@gmail.com"
+        self.email_register = "user1@gmail.com"
+        self.email_login = "use2@gmail.com"
         self.first_name = "test"
         self.last_name = "test"
         self.password = "ZYHzyh1217"
@@ -12,9 +15,22 @@ class TestViews(TestCase):
         self.street = "nyu"
         self.state = "nyu"
         self.country = "country"
-        self.zip = zip
+        self.zip = 10005
         self.long = 1
         self.lat = 1
+        self.test_user = CustomUser.objects.create_user(
+            email=self.email_login,
+            first_name=self.first_name,
+            last_name=self.last_name,
+            password=self.password,
+            gender=self.gender,
+            street=self.street,
+            state=self.state,
+            country=self.country,
+            zip=self.zip,
+            long=self.long,
+            lat=self.lat,
+        )
 
     def test_register_view_get(self):
         response = self.client.get(reverse("users:register"))
@@ -25,7 +41,7 @@ class TestViews(TestCase):
         response = self.client.post(
             reverse("users:register"),
             data={
-                "email": self.email,
+                "email": self.email_register,
                 "first_name": self.first_name,
                 "last_name": self.last_name,
                 "gender": self.gender,
@@ -37,7 +53,7 @@ class TestViews(TestCase):
         response = self.client.post(
             reverse("users:register"),
             data={
-                "email": self.email,
+                "email": self.email_register,
                 "first_name": self.first_name,
                 "last_name": self.last_name,
                 "password1": self.password,
@@ -54,32 +70,109 @@ class TestViews(TestCase):
 
     def test_login_view_post(self):
         self.client.post(
-            reverse("users:register"),
-            data={
-                "email": self.email,
-                "first_name": self.first_name,
-                "last_name": self.last_name,
-                "gender": self.gender,
-                "password1": self.password,
-                "password2": self.password,
-            },
-        )
-        self.client.post(
             reverse("users:login"),
-            data={"email": self.email, "password": self.password},
+            data={"email": self.email_login, "password": self.password},
         )
+
+    def test_default(self):
+        response = self.client.login(
+            email=self.test_user.email, password=self.test_user.password
+        )
+        print("this is response")
+        print(response)
 
     def test_set_location_get(self):
         response = self.client.get(reverse("users:set_location", kwargs={"user_id": 1}))
-
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, "users/set_location.html")
 
-    def test_pricing_view_get_unauth(self):
-        response = self.client.get(reverse("users:pricing"))
+    def test_set_location_post_valid(self):
+        # login
+        self.client.post(
+            reverse("users:login"),
+            data={"email": self.email_login, "password": self.password},
+        )
+        # location page
+        p = reverse("users:set_location", kwargs={"user_id": 1})
+        self.client.post(
+            path=p,
+            data={
+                "country": self.country,
+                "lat": self.lat,
+                "long": self.long,
+                "state": self.state,
+                "street": self.street,
+                "zip": self.zip,
+            },
+        )
 
-        self.assertEquals(response.status_code, 302)
-        # self.assertTemplateUsed(response, 'authentication/pricing.html')
+    def test_set_location_post_invalid(self):
+        # login
+        self.client.post(
+            reverse("users:login"),
+            data={"email": self.email_login, "password": self.password},
+        )
+        # location page
+        p = reverse("users:set_location", kwargs={"user_id": 1})
+        self.client.post(
+            path=p,
+            data={
+                "country": self.country,
+                "lat": self.lat,
+                "long": self.long,
+                "state": self.state,
+                "street": self.street,
+                "zip": "xxx",
+            },
+        )
+
+    def test_set_location_post_url_param(self):
+        # login
+        self.client.post(
+            reverse("users:login"),
+            data={"email": self.email_login, "password": self.password},
+        )
+        # location page
+        p = reverse("users:set_location", kwargs={"user_id": 2})
+        self.client.post(
+            path=p,
+            data={
+                "country": self.country,
+                "lat": self.lat,
+                "long": self.long,
+                "state": self.state,
+                "street": self.street,
+                "zip": "xxx",
+            },
+        )
+
+    def test_pricing_view_get(self):
+        # not login
+        response = self.client.get(reverse("users:pricing"))
+        self.assertRedirects(response, reverse("users:index"))
+        # login
+        self.client.post(
+            reverse("users:login"),
+            data={"email": self.email_login, "password": self.password},
+        )
+        # get
+        response = self.client.get(reverse("users:pricing"))
+        self.assertTemplateUsed(response, "users/pricing.html")
+
+        # post wrong param
+        response = self.client.post(
+            reverse("users:pricing"),
+            data={"tier": "asdfas"},
+        )
+        self.assertTemplateUsed(response, "users/pricing.html")
+        # post right param
+        response = self.client.post(
+            reverse("users:pricing"),
+            data={"tier": 1},
+        )
+        self.assertRedirects(response, reverse("users:index"))
+        user = CustomUser.objects.get(email=self.email_login)
+        assert user.tier == 1
 
     def test_homepage_view(self):
         response = self.client.get(reverse("users:index"))
@@ -88,11 +181,93 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, "users/homepage.html")
 
     def test_logout_view(self):
-        response = self.client.get(reverse("users:index"))
+        # login
+        self.client.post(
+            reverse("users:login"),
+            data={"email": self.email_login, "password": self.password},
+        )
 
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, "users/homepage.html")
+        response = self.client.get(reverse("users:logout"))
+        self.assertEquals(response.status_code, 302)
+        # self.assertTemplateUsed(response, "users/homepage.html")
 
     def test_search_view(self):
         response = self.client.get(reverse("users:search"))
         self.assertEquals(response.status_code, 200)
+        #  if a user doesn't have lat or long
+        CustomUser.objects.create_user(
+            email="demo@demo.com",
+            first_name=self.first_name,
+            last_name=self.last_name,
+            password=self.password,
+            gender=self.gender,
+            street=self.street,
+            state=self.state,
+            country=self.country,
+            zip=self.zip,
+        )
+        response = self.client.get(reverse("users:search"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_search_hardware_view(self):
+        response = self.client.get(reverse("users:search_hardware"))
+        self.assertEquals(response.status_code, 200)
+        #  if a user doesn't have lat or long
+        CustomUser.objects.create_user(
+            email="demo@demo.com",
+            first_name=self.first_name,
+            last_name=self.last_name,
+            password=self.password,
+            gender=self.gender,
+            street=self.street,
+            state=self.state,
+            country=self.country,
+            zip=self.zip,
+        )
+        response = self.client.get(reverse("users:search_hardware"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_profile_view(self):
+        # didn't login
+        response = self.client.get(reverse("users:profile"))
+        self.assertEquals(response.status_code, 302)
+
+        # login
+        self.client.post(
+            reverse("users:login"),
+            data={"email": self.email_login, "password": self.password},
+        )
+        response = self.client.get(reverse("users:profile"))
+        self.assertTemplateUsed(response, "users/profile.html")
+
+    def test_profile_editor_view(self):
+        print("profile_editor_view")
+        # didn't login
+        response = self.client.get(reverse("users:profile_editor"))
+        self.assertEquals(response.status_code, 302)
+
+        # login
+        self.client.post(
+            reverse("users:login"),
+            data={"email": self.email_login, "password": self.password},
+        )
+        # get request
+        response = self.client.get(reverse("users:profile_editor"))
+        self.assertTemplateUsed(response, "users/profile_editor.html")
+
+        # post request
+
+    def test_request_service_view(self):
+        # didn't login
+        response = self.client.get(reverse("users:request_service"))
+        self.assertEquals(response.status_code, 302)
+
+        # login
+        self.client.post(
+            reverse("users:login"),
+            data={"email": self.email_login, "password": self.password},
+        )
+        response = self.client.get(reverse("users:request_service"))
+        self.assertTemplateUsed(response, "users/request_services.html")
+
+    # def test_activate(self):
