23d976e4319d26e0c8449918bdf099302dcffd9f
Yuhao Zhang <yz7726@nyu.edu>
2022-04-12 05:20:51 -0400

    Update settings.py

--- home_fix/home_fix/settings.py
+++ home_fix/home_fix/settings.py
@@ -157,7 +157,7 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
-SITE_ID = 5
+SITE_ID = 4
 LOGIN_REDIRECT_URL = "/users/register/"
 
 # Additional configuration settings
