2cf3737bd3fd814e459d679f63a9c2bbefb00736
bc3177 <90463880+bc3177@users.noreply.github.com>
2022-04-26 11:21:08 -0400

    Auto Logout and Custom404

--- home_fix/basic/views.py
+++ home_fix/basic/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Homepage
 def homepage_view(request):
     return render(request, "basic/homepage.html")
+
+
+def handle_not_found(request, exception):
+    return render(request, "basic/notfound.html")
